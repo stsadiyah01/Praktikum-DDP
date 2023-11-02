1124970b60faf87ceb4a0ee2927c8333ff9ac25c
stsadiyah01 <146051617+stsadiyah01@users.noreply.github.com>
2023-11-02 19:39:52 +0700

    Add files via upload

---
+++ pekan6_SitiSa'diyah_0110123108_SI01/tugas6.2.py
@@ -0,0 +1,6 @@
+# Tugas 2
+# output : 1 + 3 + 5 + 7 + 9 + 11 + 13 + 15 + 17 + 19 = 100
+hasil = 0
+for i in range(19,0,-2) :
+    hasil += i
+print(hasil)
---
+++ pekan6_SitiSa'diyah_0110123108_SI01/tugas6.3.py
@@ -0,0 +1,7 @@
+# Tugas 3 jumlah baris5
+for i in range(1,11) :
+    for j in range(1,i+1) :
+        k = "*"
+        print(k, end="")
+    print()    
+       
